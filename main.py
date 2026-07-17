a297fbbf8ae214e7cbbba649671884b41afa1946
advyy100i <advaygujar2005@gmail.com>
2026-07-17 08:00:17 +0530

    Allow all Vercel preview URLs in CORS

--- main.py
+++ main.py
@@ -311,6 +311,7 @@ _cors_origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=_cors_origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
